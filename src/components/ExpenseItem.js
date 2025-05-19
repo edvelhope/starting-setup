52d0d2fc4cf8571e96520d238b68ccf3ba34ca33
Edoardo <edoardoderosaa@gmail.com>
2025-05-19 19:10:52 +0200

    object destructuring per passare props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
-import "./ExpenseItem.css";
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+function ExpenseItem({date, title, amount}) {
+
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">€{props.amount}</div>
+    <div className='expense-item'>
+      <div>{date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>€{amount}</div>
       </div>
     </div>
   );
